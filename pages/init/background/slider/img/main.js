54f151e6ff61a0b01366f795651d0120fcd8f724
Philippe <philippe.idalgoprestes@gmail.com>
2024-09-11 17:15:09 -0300

    m

--- pages/init/background/slider/img/main.js
+++ pages/init/background/slider/img/main.js
@@ -1,6 +1,6 @@
 export default function img(){
     const img = document.createElement("img")
     img.className = "height100 opacity05"
-    img.src = "https://www.draxter077.github.io/ouvidoriasPublicas/stockholmPalace.jpg"
+    img.src = "https://draxter077.github.io/ouvidoriasPublicas/stockholmPalace.jpg"
     return(img)
 }
