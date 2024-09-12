fc350bdd1d52d8c24839590e34c9c843ca264cf3
Philippe <philippe.idalgoprestes@gmail.com>
2024-09-12 16:14:23 -0300

    m

--- pages/init/content/body/main.js
+++ pages/init/content/body/main.js
@@ -1,10 +1,8 @@
-import menu from "./slider/leftSide/menu/main.js"
 import slider from "./slider/main.js"
 
 export default function body(){
     const body = document.createElement("div")
-    body.className = "width100 height100"
-    //body.className = "width80 height100 flexRow alignItemsCenter justifyContentFlexEnd animationFadeInDelay2 margin20px0px0px0px"
+    body.className = "width100 height100 animationFadeInDelay2"
     body.appendChild(slider())
     return(body)
 }
