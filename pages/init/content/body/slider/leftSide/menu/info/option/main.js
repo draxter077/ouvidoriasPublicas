5771144499a5e4773d1ab875c9991b197f21ebb5
Philippe <philippe.idalgoprestes@gmail.com>
2024-09-13 17:15:14 -0300

    menu

--- pages/init/content/body/slider/leftSide/menu/info/option/main.js
+++ pages/init/content/body/slider/leftSide/menu/info/option/main.js
@@ -1,6 +1,6 @@
 export default function option(d){
     const option = document.createElement("div")
-    option.className = "hColorWhiteToColorBlue fontWeight900 fontSize18px"
+    option.className = "hColorWhiteToColorBlue fontWeight900 fontSize18px rMarginNoneToMargin0px0px10px0px"
     option.innerHTML = d
     return(option)
 }
