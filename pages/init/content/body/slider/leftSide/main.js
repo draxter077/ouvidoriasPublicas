621547b236862d22f4f7cc04ad73ad09aee6b612
Philippe <philippe.idalgoprestes@gmail.com>
2024-09-13 14:00:14 -0300

    menu ahcnage

--- pages/init/content/body/slider/leftSide/main.js
+++ pages/init/content/body/slider/leftSide/main.js
@@ -2,7 +2,7 @@ import menu from "./menu/main.js"
 
 export default function leftSide(){
     const leftSide = document.createElement("div")
-    leftSide.className = "width50 height100 flexRow alignItemsCenter justifyContentFlexEnd padding20px"
+    leftSide.className = "width50 height100 flexColumn alignItemsCenter justifyContentFlexEnd padding20px"
     leftSide.appendChild(menu())
     return(leftSide)
 }
--- pages/init/content/body/slider/leftSide/menu/main.js
+++ pages/init/content/body/slider/leftSide/menu/main.js
@@ -2,8 +2,8 @@ import option from "./option/main.js"
 
 export default function menu(){
     const menu = document.createElement("div")
-    menu.className = "backgroundColorWhite padding20px25px borderRadius15px fontSize25px"
-    let ops = ["Eventos", "Notícias", "Publicações", "Links", "Bibliografia"]
+    menu.className = "width100 rFlexRowToFlexColumn justifyContentSpaceBetween padding20px25px borderRadius15px fontSize25px"
+    let ops = ["Sobre nós", "Ombudsman no mundo", "Eventos", "Notícias", "Publicações", "Links", "Bibliografia"]
     for(let i = 0; i < ops.length; i++){
         menu.appendChild(option(ops[i]))
     }
