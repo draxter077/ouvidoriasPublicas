e009fb42da1bbd524b0aeb5c225a45639bc54d8e
Philippe <philippe.idalgoprestes@gmail.com>
2024-09-13 18:01:37 -0300

    going

--- pages/init/content/body/slider/leftSide/menu/prior/option/ombudsmanMundo/main.js
+++ pages/init/content/body/slider/leftSide/menu/prior/option/ombudsmanMundo/main.js
@@ -1,5 +1,10 @@
 export default function ombudsmanMundo(){
     const ombudsmanMundo = document.createElement("div")
     ombudsmanMundo.innerHTML = "ombdsman no mundo"
+    ombudsmanMundo.onclick = function a(){
+        let d = document.getElementById("1")
+        d.style.transition = "translate var(--transitionTime)"
+        d.style.translate = "0%"
+    }
     return(ombudsmanMundo)
 }
--- pages/init/content/body/slider/rightSide/main.js
+++ pages/init/content/body/slider/rightSide/main.js
@@ -1,9 +1,6 @@
-import option from "./option/main.js"
-
 export default function rightSide(){
     const rightSide = document.createElement("div")
     rightSide.className = "width50 height100 padding20px"
     rightSide.id = "rightSide"
-    rightSide.appendChild(option())
     return(rightSide)
 }
