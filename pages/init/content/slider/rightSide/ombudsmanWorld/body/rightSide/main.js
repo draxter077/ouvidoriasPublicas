61b9f47eb3dcb3b21c97c3d9e51ae85f3215bbc5
Philippe <philippe.idalgoprestes@gmail.com>
2024-09-25 14:30:22 -0300

    bla bla bla

--- pages/init/content/slider/rightSide/ombudsmanWorld/body/rightSide/main.js
+++ pages/init/content/slider/rightSide/ombudsmanWorld/body/rightSide/main.js
@@ -1,11 +1,25 @@
+import option from "./option/main.js"
+
 export default function rightSide(){
     document.getElementsByTagName("style")[0].innerHTML += `
         .initContentSliderRightSideOmbudsmanWorldBodyRightSide{
             width: 50%;
+            height: 90%;
+            padding: 10px;
+            overflow: scroll;
+        }
+        @media screen and (max-width: 1000px){
+            .initContentSliderRightSideOmbudsmanWorldBodyRightSide{
+                width: 100%;
+                height: fit-content;
+            }
         }`
 
     const rightSide = document.createElement("div")
     rightSide.className = "initContentSliderRightSideOmbudsmanWorldBodyRightSide"
-    rightSide.innerHTML = "right"
+    const cs = ["País", "País", "País", "País", "País", "País", "País", "País", "País", "País", "País", "País", "País", "País", "País", "País", "País", "País", "País", "País", "País", "País", "País", "País", "País"]
+    for(let i = 0; i < cs.length; i++){
+        rightSide.appendChild(option(cs[i] + i))
+    }
     return(rightSide)
 }
---
+++ pages/init/content/slider/rightSide/ombudsmanWorld/body/rightSide/option/main.js
@@ -0,0 +1,15 @@
+export default function option(t){
+    document.getElementsByTagName("style")[0].innerHTML += `
+        .initContentSliderRightSideOmbudsmanWorldBodyRightSideOption{
+            padding: 10px;
+            background: var(--colorBlue);
+            border-radius: 15px;
+            text-align: cente;
+            margin: 0px 0px 10px 0px;
+            color: var(--colorWhite);
+        }`
+    const option = document.createElement("div")
+    option.className = "initContentSliderRightSideOmbudsmanWorldBodyRightSideOption"
+    option.innerHTML = t
+    return(option)
+}
