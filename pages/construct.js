6f116a69a4725e98119ae1d7e639214aba1ded7f
Philippe <philippe.idalgoprestes@gmail.com>
2024-10-11 15:16:36 -0300

    humm

--- pages/construct.js
+++ pages/construct.js
@@ -5,16 +5,5 @@ export default async function construct(){
     const root = document.getElementById("root")
     const atts = window.location.href.split("?")[1]
     root.innerHTML = ""
-    if(atts != undefined){
-        const attsType = atts.split("=")[0]
-        const attsValue = atts.split("=")[1]
-        if(attsType == "p"){
-            if(attsValue == "forms"){
-                root.appendChild(forms())
-            }
-        }
-    }
-    else{
-        root.appendChild(init())
-    }
+    root.appendChild(init())
 }
