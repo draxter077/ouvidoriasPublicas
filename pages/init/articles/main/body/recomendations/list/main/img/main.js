2eb2e5bc6a23113dabba5a4d6b2b9c0ad8dd125c
philippe <philippe.idalgoprestes@gmail.com>
2024-12-13 16:01:27 -0300

    img

---
+++ pages/init/articles/main/body/recomendations/list/main/img/main.js
@@ -0,0 +1,13 @@
+export default function img(){
+    let style = `
+        {
+            width: 100%;
+            height: 100%;   
+            border-radius: 5px;
+            box-shadow: 0px 0px 3px 0px var(--colorBlack);
+        }`
+
+    const img = createElementToPage(undefined, "img", style)
+    img.src = "https://lekaneventos.com.br/wp-content/uploads/2021/10/lekaneventos.com.br-voce-sabe-o-que-e-um-evento-corporativo-d61-1898-768x513.jpg"
+    return(img)
+}
--- pages/init/articles/main/body/recomendations/list/main/main.js
+++ pages/init/articles/main/body/recomendations/list/main/main.js
@@ -1,13 +1,14 @@
+import img from "./img/main.js"
+
 export default function main(){
     let style = `
         {
             width: 100%;
             height: 200px;
-            background: red;
             margin: 0px 0px 20px 0px;
         }`
 
     const main = createElementToPage(undefined, "div", style)
-    main.innerHTML = "Principal"
+    main.appendChild(img())
     return(main)
 }
