e8f50aaf3ffdf64328abeba10ae3222e632c265d
philippe <philippe.idalgoprestes@gmail.com>
2024-12-18 15:17:46 -0300

    news

--- pages/init/news/main/body/recomendations/list/news.js
+++ pages/init/news/main/body/recomendations/list/news.js
@@ -3,39 +3,39 @@ export default
     "main":{
         "title":"Ouvidora-geral do Ministério Público do Amazonas é admitida como sócia ativa do Instituto Latinoamericano de Ombudsman",
         "date":"26/11/2024",
-        "location":"",
+        "location":"Brasil",
         "link":"https://www.mpam.mp.br/noticias-portal/17730-ouvidora-geral-do-ministerio-publico-do-amazonas-e-admitida-como-socia-ativa-do-instituto-latinoamericano-de-ombudsman",
         "img":"https://www.mpam.mp.br/images/imprensa/imagens/OuvidoriaIS%C3%B3ciaLODSelo_eacb2.png"
     },
     "list":[
         {
-            "title":"recomendação número 1",
-            "date":"01/01/2024 a 31/12/2024",
-            "location":"Brasil/BR",
-            "link":"https://www.google.com"
+            "title":"CGU promove debate sobre ouvidorias públicas e participação social em evento na ANTT",
+            "date":"10/10/2024",
+            "location":"Brasil",
+            "link":"https://www.gov.br/cgu/pt-br/assuntos/noticias/2024/10/cgu-promove-debate-sobre-ouvidorias-publicas-e-participacao-social-em-evento-na-antt"
         },
         {
             "title":"recomendação número 2",
             "date":"01/01/2024 a 31/12/2024",
-            "location":"Brasil/BR",
+            "location":"Brasil",
             "link":"https://www.google.com"
         },
         {
             "title":"recomendação número 3",
             "date":"01/01/2024 a 31/12/2024",
-            "location":"Brasil/BR",
+            "location":"Brasil",
             "link":"https://www.google.com"
         },
         {
             "title":"recomendação número 4",
             "date":"01/01/2024 a 31/12/2024",
-            "location":"Brasil/BR",
+            "location":"Brasil",
             "link":"https://www.google.com"
         },
         {
             "title":"recomendação número 5",
             "date":"01/01/2024 a 31/12/2024",
-            "location":"Brasil/BR",
+            "location":"Brasil",
             "link":"https://www.google.com"
         }
     ]
